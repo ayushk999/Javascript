82eab01dd85874542106b066bfd35cff82119e9f
Ayush Khosla <139046771+ayushk999@users.noreply.github.com>
2024-08-30 07:42:53 +0000

    arrays_basics

---
+++ 01_basics/arrays.js
@@ -0,0 +1,19 @@
+const myArr = [0,1,2,3,4,5]
+const myHeroes = ["Batman", "Superman"]
+
+const myArr2 = new Array(1,2,3,4)
+// console.log(myArr2[2]); 
+
+// myArr.push(6)
+// myArr.pop()
+
+// myArr.unshift(7)
+// myArr.shift()
+
+// console.log(myArr.indexOf(9)); 
+// console.log(myArr.includes(9)); 
+
+const newArr = myArr.join()
+console.log(newArr); 
+console.log(myArr); 
+
