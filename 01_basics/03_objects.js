c1c9451cbfa480d12707375f4f7fd721b39ef350
Ayush Khosla <139046771+ayushk999@users.noreply.github.com>
2024-08-30 09:34:38 +0000

    objects

---
+++ 01_basics/03_objects.js
@@ -0,0 +1,13 @@
+const JsUser = {
+    name: "Ayush",
+    "full name": "Ayush Khosla",
+    age: 22,
+    location: "Punjab",
+    email: "ayushkhosla31@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser.email);
+console.log(JsUser["full name"]);
+
