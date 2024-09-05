6e0b50c8c01300b347c4e43a1b5c3b67923cb0e3
Ayush Khosla <139046771+ayushk999@users.noreply.github.com>
2024-09-05 10:45:45 +0000

    objects final

--- 01_basics/03_objects.js
+++ 01_basics/03_objects.js
@@ -1,6 +1,9 @@
+const mySym = Symbol("key1")
+
 const JsUser = {
     name: "Ayush",
     "full name": "Ayush Khosla",
+    [mySym] : "mykey1",
     age: 22,
     location: "Punjab",
     email: "ayushkhosla31@gmail.com",
@@ -8,6 +11,23 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log(JsUser.email);
-console.log(JsUser["full name"]);
+// console.log(JsUser.email);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "Ayush@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "Ayush@company.com"
+// console.log(JsUser);
+
+JsUser.greeting = function() {
+    console.log("Hello");   
+}
+JsUser.greetingTwo = function() {
+    console.log(`Hello, ${this.name}`);   
+}
+console.log(JsUser.greeting()); 
+console.log(JsUser.greetingTwo());
+
+
 
---
+++ 01_basics/04_objects.js
@@ -0,0 +1,50 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id  = "123abc"
+tinderUser.name  = "ayush"
+tinderUser.isLoggedIn  = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "ayush",
+    fullname: {
+        userfullname: {
+            firstname: "Ayush",
+            lastname: "Khosla"
+        }
+    }
+}
+
+// console.log(regularUser.fullname?.userfullname?.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+// const obj3 = Object.assign({}, obj1, obj2)
+// const obj3 = {...obj1, ...obj2}
+
+// console.log(obj3);
+// console.log({...obj1, ...obj2});
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "JS",
+    price: "999",
+    courseInstructor: "Ayush"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+
+
